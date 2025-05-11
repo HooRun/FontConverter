42706882e854f23e1beebae42036ba6197b5fb24
Hossein Bakhshipoor <h.bakhshipoor@gmail.com>
2025-05-11 14:16:12 +0330

    blazor

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: zMWncpi9 */
+/* Manifest version: c97svAnB */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
