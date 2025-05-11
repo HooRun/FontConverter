84863cbc3a20baafb7c051a2342d6a035ac8e376
Hossein Bakhshipoor <h.bakhshipoor@gmail.com>
2025-05-11 10:28:19 +0330

    Add Blazor WASM

--- FontConverter.Library/Models/LVGL/FontDetails/LVGLFontGlyph.cs
+++ FontConverter.Library/Models/LVGL/FontDetails/LVGLFontGlyph.cs
@@ -11,9 +11,6 @@ public class LVGLFontGlyph
 {
     public LVGLFontGlyph()
     {
-        Name = string.Empty;
-        Description = string.Empty;
-        Bitmap = [];
         Unicodes = [];
 
         IsEmpty = false;
@@ -22,29 +19,17 @@ public class LVGLFontGlyph
 
     }
 
-    public string Name { get; set; }
-    public string Description { get; set; }
-
     public int Index { get; set; }
-    public int AdvanceWidth { get; set; }
-    public int Width { get; set; }
-    public int Height { get; set; }
-    public int OffsetX { get; set; }
-    public int OffsetY { get; set; }
+    
+    public LVGLGlyphProperties GlyphProperties { get; set; } = new();
+    public LVGLFontAdjusment GlyphAdjusment { get; set; } = new();
+    public LVGLGlyphBitmap GlyphBitmap { get; set; } = new();
 
-    public int LineHeight { get; set; }
-    public int BaseLine { get; set; }
-    public int MaxCharWidth { get; set; }
-    public int YAxisPosition { get; set; }
+    
 
-    public byte BitsPerPixel { get; set; }
-    public int Threshold {  get; set; }
-    public int Gamma { get; set; }
+    
 
-    public byte[] Bitmap { get; set; }
-    public int BitmapSize { get; set; }
-    public int BitmapWidth { get; set; }
-    public int BitmapHeight { get; set; }
+    
     
     public List<(uint CodePoint, string Name)> Unicodes { get; set; }
 
---
+++ FontConverter.Library/Models/LVGL/FontDetails/LVGLGlyphProperties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVGLFontConverter.Library.Models;
+
+public class LVGLGlyphProperties
+{
+    public LVGLGlyphProperties()
+    {
+        
+    }
+
+    public LVGLGlyphProperties(string name, string description, int advanceWidth, int width, int height, int offsetX, int offsetY) : this()
+    {
+        Name = name;
+        Description = description;
+        AdvanceWidth = advanceWidth;
+        Width = width;
+        Height = height;
+        OffsetX = offsetX;
+        OffsetY = offsetY;
+    }
+
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int AdvanceWidth { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int OffsetX { get; set; }
+    public int OffsetY { get; set; }
+}
